6d784207a3fd2da5dda1cd5d62ba5f097fb543ed
shiqiwang <shiqi.wang@live.com>
2017-11-21 16:07:25 +0800

    router

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Home from './view/home'
+import VueRouter from './router'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  }
+]
+
+const router = new VueRouter(routes)
+export default {
+  name: 'app',
+  router,
+  component: Home
+}
+</script>
+
---
+++ src/view/home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div><hello></hello></div>
+</template>
+
+<script>
+  import Hello from 'components/hello'
+  export default {
+    components: {Hello}
+  }
+</script>
